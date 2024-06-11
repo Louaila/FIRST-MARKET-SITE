f30a487faa2b01657bf174f89396c30b30a399b3
louaila <lou591@hotmail.fr>
2024-06-11 16:41:39 +0200

    modif js

--- js/scripts.js
+++ js/scripts.js
@@ -7,35 +7,75 @@
 // Use this file to add JavaScript to your project
 
 //incrementer le bouton//
+
 let nb_items = 0;
+let cart = [];
+
 const boutons = document.getElementsByClassName("btn_add_to-cart");
 const displayNbOfItems = document.getElementById("cart-nb-items");
+const total = document.getElementById("totalPrice");
+
+// interation utilisateur //
 
 for (let i = 0; i < boutons.length; i++) {
   const bouton = boutons[i];
   bouton.addEventListener("click", add);
 }
 
-function add(event) {
+// mis à jour de l'affichage
 
+function displayMyCart() {
+  document.getElementById("myList").innerHTML = "";
+  let totalPrice = 0;
+
+  cart.forEach((item) => {
+    const productLi = document.createElement("li");
+    const productLiText = document.createTextNode(item.name + " ");
+    const liTotalPrice = item.price * item.quantity;
+    const productLiPriceText = document.createTextNode(
+      liTotalPrice.toFixed(2) + " "
+    );
+    const productLiquantityText = document.createTextNode(item.quantity);
+
+    productLi.appendChild(productLiText);
+    productLi.appendChild(productLiPriceText);
+    productLi.appendChild(productLiquantityText);
+    document.getElementById("myList").appendChild(productLi);
+
+    productLi.appendChild(productLiText);
+    document.getElementById("myList").appendChild(productLi);
+    totalPrice += item.price * item.quantity;
+  });
+
+  document.getElementById("cart-nb-items").innerHTML = cart.length;
+
+  const litotal = document.createElement("li");
+  const litotaltext = document.createTextNode(
+    "Total: " + totalPrice.toFixed(2) + " €"
+  );
+  litotal.appendChild(litotaltext);
+  document.getElementById("myList").appendChild(litotal);
+}
+
+//  mise à jour du stockage
+
+function add(event) {
   const originButton = event.target;
   const productLabel = originButton.dataset.product;
+  const productPrice = parseFloat(originButton.dataset.price);
 
-  // on met a jour le nom d'élément dans le panier//
-  nb_items++;
-  displayNbOfItems.innerHTML = nb_items;
+  const existingProduct = cart.find((item) => item.name === productLabel);
 
-  //ajouter un produit à la liste //
-  const node = document.createElement("li");
-  // node = <li>myList</li>
-  const textnode = document.createTextNode(productLabel);
-  node.appendChild(textnode);
+  if (existingProduct) {
+    existingProduct.quantity += 1;
+  } else {
+    const product = {
+      name: productLabel,
+      price: productPrice,
+      quantity: 1,
+    };
 
-  document.getElementById("myList").appendChild(node);
-
-  const dropdownList = document.getElementById("myList");
-  const existingItem = dropdownList.querySelector("#firstLiDropdown");
-  if (existingItem) {
-      dropdownList.removeChild(existingItem);
+    cart.push(product);
   }
+  displayMyCart();
 }
